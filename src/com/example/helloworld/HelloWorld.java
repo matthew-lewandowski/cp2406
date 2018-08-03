74a828cff4f83738ca1aab638b9e6771d512ada2
matthew-lewandowski <37138972+matthew-lewandowski@users.noreply.github.com>
2018-08-03 11:42:21 +1000

    prac_01

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Matthew Lewandowski on 03/08/2018.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
